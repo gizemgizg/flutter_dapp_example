4399da8715d9a56e715276fd0f36535fa3864fc5
Gizem Malçok <gzm.mlck94@gmail.com>
2023-03-08 22:48:40 +0300

    ADD smart contract added

---
+++ contracts/UserContract.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.2 <0.9.0;
+
+contract UsersContract {
+    uint256 public userCount = 0;
+
+    struct User {
+        uint256 id;
+        string name;
+        string surname;
+    }
+
+    mapping(uint256 => User) public users;
+
+    event UserCreated(uint256 id, string name, string surname);
+    event UserDeleted(uint256 id);
+
+    function createUser(string memory _name, string memory _surname) public {
+            users[userCount] = User(userCount, _name, _surname);
+            emit UserCreated(userCount, _name, _surname);
+            userCount++;
+    }
+
+    function deleteUser(uint256 _id) public{
+        delete users[_id];
+        emit UserDeleted(_id);
+        userCount--;
+    }
+}
---
+++ truffle-config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "http://127.0.0.1",
+      port: 7545,
+      network_id: "5777",
+    },
+  },
+  contracts_directory: "./contracts",
+  compilers: {
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+
+  db: {
+    enabled: false,
+  },
+};
